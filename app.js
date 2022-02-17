714398e08258410b7310f42f4d43891c208cacad
Cole <cole.smith.code@gmail.com>
2022-02-17 12:15:11 -0700

    added a function to display the split letters

--- app.js
+++ app.js
@@ -17,8 +17,22 @@ const phrases = [
 // Grabs a random phrase to be split into an array of characters
 const getRandomPhraseAsArray = (arr) => {
     let getLetter = math.floor(math.random() * arr.length)
-    let phrases = arr[getLetter]
+    let phrase = arr[getLetter]
 
-    return phrases.split("");
+    return phrase.split("");
 };
+// 
+const addPhraseToDisplay = (arr) => {
+    for(let char of arr) {
+        const li = document.createElement("li")
+        li.innerHTML = char;
+        if (char === " ") {
+            li.classList.add("space")
+        } else {
+            li.classList.add("letter")
+        }
+        phrase.append(li)
+    }
+
+}
 
