220f252b64061dd9c2143c362705c622de8da812
Cole <cole.smith.code@gmail.com>
2022-02-17 11:24:16 -0700

    declared the starting variables

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 const qwerty = document.querySelector('#qwerty')
 const buttons = document.querySelectorAll('#qwerty button')
 const phrase = document.querySelector('#phrase ul')
+const startGame = document.querySelector('.btn__reset')
+const overlay = document.querySelector('#overlay')
+let hearts = document.querySelectorAll('.tries')
+let missed = 0;
