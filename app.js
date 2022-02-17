d9ab9749e9964bb946a71060eb93c5bbcefd9a06
Cole <cole.smith.code@gmail.com>
2022-02-17 12:19:41 -0700

    small change

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const startGame = document.querySelector('.btn__reset')
 const overlay = document.querySelector('#overlay')
 let hearts = document.querySelectorAll('.tries')
 let missed = 0;
+
 // An array that holds phrases to be split later on
 const phrases = [
     "live free die young",
@@ -14,6 +15,7 @@ const phrases = [
     "see what i could pull",
     "im strange",
 ]
+
 // Grabs a random phrase to be split into an array of characters
 const getRandomPhraseAsArray = (arr) => {
     let getLetter = math.floor(math.random() * arr.length)
@@ -21,15 +23,16 @@ const getRandomPhraseAsArray = (arr) => {
 
     return phrase.split("");
 };
-// 
+
+// Loops through the array of characters to see which are spaces and which are letters
 const addPhraseToDisplay = (arr) => {
     for(let char of arr) {
         const li = document.createElement("li")
         li.innerHTML = char;
-        if (char === " ") {
-            li.classList.add("space")
-        } else {
+        if (char === "letter") {
             li.classList.add("letter")
+        } else {
+            li.classList.add("space")
         }
         phrase.append(li)
     }
