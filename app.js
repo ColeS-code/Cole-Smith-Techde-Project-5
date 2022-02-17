9e1e47cd1a9daa9fb7f3307e9a897697d3c7cd23
Cole <cole.smith.code@gmail.com>
2022-02-17 11:31:38 -0700

    few comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Declared the necessary variables, connected to the HTML
 const qwerty = document.querySelector('#qwerty')
 const buttons = document.querySelectorAll('#qwerty button')
 const phrase = document.querySelector('#phrase ul')
@@ -6,12 +6,13 @@ const startGame = document.querySelector('.btn__reset')
 const overlay = document.querySelector('#overlay')
 let hearts = document.querySelectorAll('.tries')
 let missed = 0;
-
+// An array that holds phrases to be split later on
 const phrases = [
     "live free die young",
     "spiderman junior",
     "gorilla gurpin",
     "see what i could pull",
     "im strange",
-
 ]
+//
+
